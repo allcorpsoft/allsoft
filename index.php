497403a2dfb4446f4b60525412ed35bdd75df842
allcorpsoft <allcorpsoft@gmail.com>
2015-08-01 09:48:28 +0300

    Перый файл

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="admin" />
+
+	<title>Создание и продвижение сайтов </title>
+</head>
+
+<body>
+
+<h1>Allsoft</h1>
+
+
+</body>
+</html>
